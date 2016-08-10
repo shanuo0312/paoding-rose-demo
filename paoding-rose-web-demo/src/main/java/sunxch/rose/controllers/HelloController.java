eb3d5a3ca594aa0982699927ec8401e8d342801a
xiaochen.sun <sunxch1003>
2016-08-10 10:54:58 +0800

    paoding-rose-demo

---
+++ paoding-rose-web-demo/src/main/java/sunxch/rose/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package sunxch.rose.controllers;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * HelloController
+ *
+ * @author xiaochen.sun
+ * @since 2016-08-09 18:05
+ */
+public class HelloController {
+    public static final Log logger = LogFactory.getLog(HelloController.class);
+
+}
